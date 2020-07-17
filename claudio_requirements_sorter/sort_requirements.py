2ef31a5b591797e42a5adf5a50df356ea02eb493
Elie S <1elie21356@gmail.com>
2020-07-17 15:54:24 -0600

    cleaning up if else with for loop

--- claudio_requirements_sorter/sort_requirements.py
+++ claudio_requirements_sorter/sort_requirements.py
@@ -10,18 +10,11 @@ def sort_requirements(requirements: List[str]) -> List[str]:
 def _get_requirements_pieces(requirements: List[str]) -> List[Tuple[str, str]]:
     requirement_pieces = []
     for requirement in requirements:
-        if (sep := "==") in requirement:
-            requirement_pieces.append(_split_requirement(requirement=requirement, sep=sep))
-        elif (sep := ">=") in requirement:
-            requirement_pieces.append(_split_requirement(requirement=requirement, sep=sep))
-        elif (sep := "<=") in requirement:
-            requirement_pieces.append(_split_requirement(requirement=requirement, sep=sep))
+        for sep in ["==", ">=", "<="]:
+            index = requirement.index(sep)
+            requirement_pieces.append((requirement[:index], requirement[index:]))
+            break
         else:
             raise ValueError(f"Invalid separator in: {requirement}")
 
     return requirement_pieces
-
-
-def _split_requirement(requirement: str, sep: str) -> tuple:
-    index = requirement.index(sep)
-    return requirement[:index], requirement[index:]
