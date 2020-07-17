398718de5eaf3b8bf7a1728af515fa88f4d85b8e
Elie S <1elie21356@gmail.com>
2020-07-17 16:13:11 -0600

    type hint fix and optimizing imports

--- claudio_requirements_sorter/cli.py
+++ claudio_requirements_sorter/cli.py
@@ -1,18 +1,15 @@
-import os
-
 import click
-from click import Path
 
 from claudio_requirements_sorter.sort_requirements import sort_requirements
 
 
 @click.command()
-@click.argument("src", type=click.File("r"))
-@click.argument("dst", type=click.File("w"))
-def main(src: str, dst: str) -> int:
-    requirements = src.read().split()
+@click.argument("source", type=click.File("r"))
+@click.argument("destination", type=click.File("w"))
+def main(source: click.File, destination: click.File) -> int:
+    requirements = source.read().split()
     new_requirements = sort_requirements(requirements=requirements)
-    dst.write("\n".join(new_requirements))
+    destination.write("\n".join(new_requirements))
 
-    click.echo(f"Successfully wrote to {dst.name}")
+    click.echo(f"Successfully wrote to {destination.name}")
     return 0
